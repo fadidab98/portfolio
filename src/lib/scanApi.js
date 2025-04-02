956c3818ddb67935a43c49364dd2d6b295517752
Fadidab <fadidab1998@gmail.com>
2025-04-02 19:28:05 +0200

    adding docker

--- src/lib/scanApi.js
+++ src/lib/scanApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const scanApi = createApi({
   reducerPath: 'scanApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://webscan.serp24.online' }), //just this if your API is hosted elsewhere
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://webscan.serp24.online' }), //jut this if your API is hosted elsewhere
   endpoints: (builder) => ({
     scanWebsite: builder.mutation({
       query: (url) => ({

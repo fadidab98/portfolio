b7862d65ab6d51fc5ccf4da1b18768dad90b5f50
Fadidab <fadidab1998@gmail.com>
2025-04-03 16:44:39 +0200

    changing style

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,9 +12,7 @@ export default function Document() {
       <meta name="description" content="Fadi Dabboura Portfolio of a Machine Learning Engineer and Frontend Developer." />
       <meta name="keywords" content="machine learning, frontend,backend, devops, Docker, nginx, reactjs, js, docker-compose, nextjs, portfolio" />
       
-      <link rel="preload" href="/fonts/Inter-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      <link rel="preload" href="/fonts/Inter-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-      <link rel="preload" href="/fonts/PlayfairDisplay-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+  
     
       {/* Existing schema script */}
     </Head>

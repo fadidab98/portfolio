747272337a1d219bc70a7ec6d064afe4455bec25
Fadidab <fadidab1998@gmail.com>
2025-04-06 19:15:58 +0200

    adding og

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,6 +27,11 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="FadiLogic" />
         <meta property="og:locale" content="en_US" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />

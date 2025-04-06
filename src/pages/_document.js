085a228bbcbb536e2ff54b15b1a92574bf521741
Fadidab <fadidab1998@gmail.com>
2025-04-06 19:09:01 +0200

    adding og

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Basic SEO Tags */}
         <meta
           name="description"
           content="Fadi Dabboura’s professional portfolio showcasing expertise as a DevOps Engineer and Full-Stack Developer."
@@ -13,8 +14,19 @@ export default function Document() {
           content="devops, full-stack, backend, software engineering, Docker, nginx, reactjs, javascript, docker-compose, nextjs, portfolio"
         />
         <meta name="author" content="Fadi Dabboura" />
-        {/* Add any global favicon or meta tags here */}
-       
+        {/* Fallback Open Graph Tags */}
+        <title>Fadi Dabboura - Portfolio & Website Scan | FadiLogic</title>
+        <meta property="og:title" content="Fadi Dabboura - Portfolio & Website Scan | FadiLogic" />
+        <meta property="og:description" content="Fadi Dabboura’s FadiLogic: Free webscan tool and portfolio of DevOps projects shared on LinkedIn and Facebook." />
+        <meta property="og:image" content="https://fadilogic.serp24.online/images/FadiLogic.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Fadi Dabboura Website Scan and Portfolio" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:url" content="https://fadilogic.serp24.online/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="FadiLogic" />
+        <meta property="og:locale" content="en_US" />
       </Head>
       <body>
         <Main />

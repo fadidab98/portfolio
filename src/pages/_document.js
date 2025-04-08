c71f4ecbcc8a4ebcd6012a4cda5b7bea7dfa9da6
Fadidab <fadidab1998@gmail.com>
2025-04-08 16:27:46 +0200

    adding changed data

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" href="https://fadilogic.serp24.online/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://fadilogic.serp24.online/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://fadilogic.serp24.online/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="https://fadilogic.serp24.online/favicon-64x64.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://fadilogic.serp24.online/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="keywords" content="fadi, dabboura, fadi dabboura, fadi website scan, website scan, webscan, web scan tool, website performance, web development, devops, portfolio, fadi dabboura portfolio, fadilogic" />     
          </Head>

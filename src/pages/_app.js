b14309204ab4a16047eb8011f63f8bc9be044aca
Fadidab <fadidab1998@gmail.com>
2025-04-10 06:39:49 +0200

    adding og

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import { store } from '../lib/store';
 import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
 const inter = Inter({ subsets: ['latin'], weights: [400, 700], display: 'swap' });
 
 export default function MyApp({ Component, pageProps }) {

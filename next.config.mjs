1bc02f2624c1535f6773369e080c45fc04508738
Fadidab <fadidab1998@gmail.com>
2025-04-11 11:13:13 +0200

    adding caching

--- next.config.mjs
+++ next.config.mjs
@@ -6,20 +6,7 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
-  async headers() {
-    return [
-      {
-        // Match static assets in /public and .next/static
-        source: '/:path*.(jpg|jpeg|png|gif|svg|ico|css|js|woff|woff2)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;

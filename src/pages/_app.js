886e4aacaca0580abe9d04ab706de0eb40677100
Fadidab <fadidab1998@gmail.com>
2025-04-21 02:47:51 +0200

    testing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -72,7 +72,6 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
-        {/* Fallback meta tags for all pages */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,6 @@ export default function Home({ projects }) {
   return (
     <>
       <Head>
-        {/* Fallback meta tags for crawlers */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
@@ -128,6 +127,31 @@ export default function Home({ projects }) {
         />
         <meta name="author" content="Fadi Dabboura" />
         <meta name="theme-color" content="#1a202c" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="64x64"
+          href="/favicon-64x64.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{

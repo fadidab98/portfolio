54ff9b4b38c53dff4d038f1156acae0c9618666f
Fadidab <fadidab1998@gmail.com>
2025-05-23 14:49:12 +0200

    testing

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -10,21 +10,12 @@ function generateSitemap() {
     { url: '/contact', lastmod: lastModified, changefreq: 'monthly', priority: '0.8' },
   ];
 
-  return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${pages
-        .map(
-          (page) => `
-            <url>
-              <loc>${baseUrl}${page.url}</loc>
-              <lastmod>${page.lastmod}</lastmod>
-              <changefreq>${page.changefreq}</changefreq>
-              <priority>${page.priority}</priority>
-            </url>
-          `
-        )
-        .join('')}
-    </urlset>`;
+  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${pages
+    .map(
+      (page) =>
+        `<url><loc>${baseUrl}${page.url}</loc><lastmod>${page.lastmod}</lastmod><changefreq>${page.changefreq}</changefreq><priority>${page.priority}</priority></url>`
+    )
+    .join('')}</urlset>`;
 }
 
 export async function GET() {

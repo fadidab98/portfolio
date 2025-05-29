4da39ad4677a0b4583a113e18e94956b75b2b54d
Fadidab <fadidab1998@gmail.com>
2025-05-29 02:33:36 +0200

    Remove .next cache files

--- next.config.ts
+++ next.config.ts
@@ -11,15 +11,14 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true, // Optional, adjust as needed
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ['image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'fadidabboura.com',
       },
     ],
-    deviceSizes: [150, 200, 250, 300, 640, 750, 828, 1080, 1200, 1920, 2048, 3840], // Ensure 300 is included
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+ 
   },
   async redirects() {
     return [];
